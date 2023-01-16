ad273544f6c12e6fc552eef6a90e0799a8431f29
apollinaurus <apollinaurus@gmail.com>
2023-01-16 14:17:59 +0400

    tasks

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.get('/')
 def helloGet():
     return 'Hello world GET'
 
+
 @app.post('/')
 def helloPost():
-    return 'Hello world POST'
+    return 'Hello world POST:{}'.format(request.data)
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
--- task.py
+++ task.py
@@ -1,9 +1,22 @@
-first_file = open("first_tour.txt", "r")
-second_file = open("second_tour.txt", "w")
+with open('first_tour.txt', 'r') as first_file:
+    players = []
+    k = int(first_file.readline().strip('\n'))
+    for line in first_file:
+        players += (line.split(),)
 
-K = first_file.readline().split()[-1]
+players.sort(key=lambda x: int(x[2]), reverse=True)
 
-for line in first_file:
-    score = line.split()[-1]
-    if score > K:
-        second_file.write(line)
+i = 0
+while i < len(players):
+    if int(players[i][2]) > k:
+        players[i] = "{}) {}. {} {}\n".format(str(i + 1), players[i][1][0], players[i][0], players[i][2])
+        i += 1
+    else:
+        del players[i:len(players)]
+        break
+number_of_passed = str(len(players))
+
+with open('second_tour.txt', 'w') as second_file:
+    second_file.write(number_of_passed)
+    second_file.write('\n')
+    second_file.writelines(players)
